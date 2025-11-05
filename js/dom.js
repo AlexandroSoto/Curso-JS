612ab61efab4ef22cc75b4ee4ee2fff1408dc4d3
Alexandro Soto <wolf.hunter.979@gmail.com>
2025-11-05 13:43:17 -0600

    BOM: propiedades y eventos. Metodos. Objetos: URL, historial y navegador

--- js/dom.js
+++ js/dom.js
@@ -299,7 +299,7 @@ $cards.after($newCard);
 */ 
 
 //-------------------- DOM: Maneajadores de Eventos --------------------------------
-
+/*
 function holaMundo(){
   alert("Hola Mundo");
   console.log(event);
@@ -324,3 +324,125 @@ $eventoMultiple.addEventListener("click",(e)=>{
   console.log(e.target);
   console.log(e);
 })
+*/
+
+//-------------------  DOM: Flujo de Eventos (Burbuja y Captura)--------------------------
+//----------------     DOM: stopPropagation & preventDefault -----------------------------
+//-------------------- DOM: Delegación de Eventos ----------------------------------------
+
+/*
+function flujoEventos(e){
+  console.log(`Hola te saluda ${this}, el clic lo origino ${e.target.className}`);
+}
+
+document.addEventListener("click", (e)=>{
+  console.log("click en " , e.target);
+
+  if(e.target.matches("eventos-flujo div")){
+    flujoEventos(e);
+  }
+
+  if(e.target.matches("eventos-flujo a")){
+    alert("Hola soy tu amigo y docente digital JonMircha");
+    e.preventDefault();
+  }
+});
+
+console.log(window.document);
+
+*/
+
+//---------------------  BOM: Propiedades y Eventos   ---------------------------
+/*
+window.addEventListener("resize",e=>{
+  console.clear();
+  console.log("***********Evento Resize***********");
+  console.log(window.innerWidth);
+  console.log(window.innerHeight);
+  console.log(window.outerWidth);
+  console.log(window.outerHeight);
+  console.log(window.scrollX);
+  console.log(window.scrollY);
+  console.log(e);
+});
+
+window.addEventListener("scroll",e=>{
+  console.clear();
+  console.log("***********Evento Scroll***********");
+  console.log(window.scrollX);
+  console.log(window.scrollY);
+  console.log(e);
+});
+
+window.addEventListener("load", e=>{
+  console.log("***********Evento Load***********");
+  console.log(window.screenX);
+  console.log(window.screenY);
+  console.log(e);
+})
+
+document.addEventListener("DOMContentLoaded",e=>{
+  console.log("***********Evento DOMContentLoaded***********");
+  console.log(window.screenX);
+  console.log(window.screenY);
+  console.log(e);
+})
+*/ 
+
+//------------------------------  BOM: Metodos  --------------------------------------
+
+//window.alert("alerta");
+//window.confirm("Confirmacion");
+//window.prompt("Aviso");
+/*
+const $btnAbrir=document.getElementById("abrir-ventana"),
+ $btnCerrar=document.getElementById("cerrar-ventana"),
+ $btnImprimir=document.getElementById("imprimir-ventana");
+
+let ventana;
+
+$btnAbrir.addEventListener('click',e=>{
+  ventana = window.open("https://jonmircha.com");
+});
+
+$btnCerrar.addEventListener('click',e=>{
+  //window.close();
+  ventana.close();
+});
+
+$btnImprimir.addEventListener('click',e=>{
+  window.print();
+});
+*/ 
+
+//-----------------  BOM: Objetos: URL, Historial y Navegador -----------------------
+
+// console.log("**********Objeto URL(location)*******************");
+// console.log(location);
+// console.log(location.origin);
+// console.log(location.protocol);
+// console.log(location.host);
+// console.log(location.hostname);
+// console.log(location.port);
+// console.log(location.href);
+// console.log(location.hash);//detecta valor de la url despues del hash
+// console.log(location.pathname);
+
+// console.log("********* Objeto Historial (history) **********");
+// console.log(history);
+// console.log(history.length);
+// history.forward(1);
+// history.go(-3);
+// history.back(2);
+
+console.log("********* Objeto Navegador (navigator) **********");
+console.log(navigator);
+console.log(navigator.connection);
+console.log(navigator.geolocation);
+console.log(navigator.mediaDevices);
+console.log(navigator.mimeTypes);
+console.log(navigator.onLine);
+console.log(navigator.serviceWorker);
+console.log(navigator.storage);
+console.log(navigator.usb);
+console.log(navigator.userAgent);
